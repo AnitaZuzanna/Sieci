b4f64b4f056d419c38f740a3c2cfbc77671d140c
Zuzanna Kosobucka <anita@kosobucka.pl>
2019-12-07 20:33:37 +0100

    Add empty client project

---
+++ client/ChatClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Client
+{
+    class ChatClient
+    {
+        static void Main(string[] args)
+        {
+            foreach(var param in args)
+            {
+                Console.WriteLine(param);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

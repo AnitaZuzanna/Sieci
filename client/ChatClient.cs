3d81c34c3bc5da690e7b0b0806e04cf72e7204dd
Zuzanna Kosobucka <anita@kosobucka.pl>
2019-12-08 14:05:56 +0100

    Add client file

--- client/ChatClient.cs
+++ client/ChatClient.cs
@@ -1,16 +1,52 @@
-﻿using System;
+﻿using System;  
+using System.Net;  
+using System.Net.Sockets;  
+using System.Text;  
+  
 
 namespace Client
 {
     class ChatClient
     {
-        static void Main(string[] args)
+        public static void StartClient(string address)
         {
-            foreach(var param in args)
+
+            byte[] rcMessage = new byte[1024];
+            byte[] sdMessage = new byte[1024];
+            int port = 9000;
+             
+            IPAddress ipAddress = IPAddress.Parse(address);
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress,port); 
+
+            Socket clientSocket = new Socket( ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            clientSocket.Connect(remoteEP);
+
+            while(true)
             {
-                Console.WriteLine(param);
+                string input = Console.ReadLine();
+                sdMessage = Encoding.ASCII.GetBytes(input);
+                int sendMessage = clientSocket.Send(sdMessage);
             }
-            Console.WriteLine("Hello World!");
+
+            clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+
+        }
+        
+        static int Main(string[] args)
+        {
+            if(args.Length > 0)
+            {
+                StartClient(args[0]);
+            }
+            else
+            {
+                Console.WriteLine("Usage: ./Client [ip address]");
+                return 1;
+            }
+
+            return 0;
         }
     }
 }
